528c5623a36d39153d363bab5ab139b686def455
Mark Saiget <saiget@gmail.com>
2015-02-15 18:20:33 -0800

    Change name back to default so lettuce works locally and on travis.

--- features/steps.py
+++ features/steps.py
@@ -97,7 +97,7 @@ def app():
     import os
 
     # settings = {'db': LOCAL_DSN}
-    os.environ['DATABASE_URL'] = TRAVIS
+    os.environ['DATABASE_URL'] = LOCAL_DSN
     app = main()
     world.app = TestApp(app)
 
