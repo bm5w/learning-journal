f1f07ad91d007a4d2721fe98541518d674cb436d
Mark Saiget <saiget@gmail.com>
2015-02-15 18:16:08 -0800

    Change name in lettuce hook for db.

--- features/steps.py
+++ features/steps.py
@@ -97,7 +97,7 @@ def app():
     import os
 
     # settings = {'db': LOCAL_DSN}
-    os.environ['DATABASE_URL'] = LOCAL_DSN
+    os.environ['DATABASE_URL'] = TRAVIS
     app = main()
     world.app = TestApp(app)
 
